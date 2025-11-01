1d5bcf1de06d79a466e37c83f9c40e81a853168a
lipenathan <felipenatthan@gmail.com>
2025-10-31 21:32:01 -0300

    get usuarios

--- src/main/java/github/lipenathan/demo/controller/UsuarioController.java
+++ src/main/java/github/lipenathan/demo/controller/UsuarioController.java
@@ -1,4 +1,27 @@
 package github.lipenathan.demo.controller;
 
+import github.lipenathan.demo.model.Usuario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class UsuarioController {
+
+    List<Usuario> usuarios = new ArrayList<>();
+
+    @PostMapping("/usuarios/novo")
+    public boolean novoUsuario(@RequestBody Usuario usuario) {
+        usuarios.add(usuario);
+        return true;
+    }
+
+    @GetMapping("/usuarios")
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
 }
