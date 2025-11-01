fd1e27e80da883173e41d1b476396344d0133675
lipenathan <felipenatthan@gmail.com>
2025-10-31 21:46:55 -0300

    atualizar usuário

--- src/main/java/github/lipenathan/demo/controller/UsuarioController.java
+++ src/main/java/github/lipenathan/demo/controller/UsuarioController.java
@@ -1,10 +1,7 @@
 package github.lipenathan.demo.controller;
 
 import github.lipenathan.demo.model.Usuario;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +9,14 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
+    private int ID = 0;
+
     List<Usuario> usuarios = new ArrayList<>();
 
     @PostMapping("/usuarios/novo")
     public boolean novoUsuario(@RequestBody Usuario usuario) {
+
+        usuario.setId(++ID);
         usuarios.add(usuario);
         return true;
     }
@@ -24,4 +25,19 @@ public class UsuarioController {
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
+
+    @PutMapping("/usuarios/atualizar")
+    public boolean aualizarUsuario(@RequestBody Usuario usuario) {
+
+        for (Usuario u : usuarios) {
+            if (u.getId() == usuario.getId()) {
+                u.setNome(usuario.getNome());
+                u.setEmail(usuario.getEmail());
+                u.setSenha(usuario.getSenha());
+            }
+        }
+
+        return true;
+    }
+
 }
