487d185dea03d499c843288065e46a105fde2ddf
lipenathan <felipenatthan@gmail.com>
2025-11-03 21:55:44 -0300

    get, put, delete + path variable e request param.

--- src/main/java/github/lipenathan/demo/controller/UsuarioController.java
+++ src/main/java/github/lipenathan/demo/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
